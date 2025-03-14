a4113d68fa95f57ce6fe23df3578673de380b8ba
Rodrigo <rodrileogabriel@gmail.com>
2025-03-14 15:17:12 -0400

    chore: Eliminando archivos no utilizados

--- apis/apirest/src/main/java/com/iruiz/apirest/Persona/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iruiz.apirest.Persona;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface PersonRepository extends JpaRepository <Persona, Integer> {
-
-}
