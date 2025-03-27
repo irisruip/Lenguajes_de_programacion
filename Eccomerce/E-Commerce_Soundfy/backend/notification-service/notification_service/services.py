0154e200901e29d591947b54f1623da371782952
EmilioUzcategui <emilioandres.jh@gmail.com>
2025-03-26 22:03:44 -0400

    ecommerce completo :b

--- Eccomerce/E-Commerce_Soundfy/backend/notification-service/notification_service/services.py
+++ Eccomerce/E-Commerce_Soundfy/backend/notification-service/notification_service/services.py
@@ -2,12 +2,16 @@ from twilio.rest import Client
 from django.conf import settings
 from django.core.mail import send_mail
 from django.http import HttpResponse
+import os 
+twilio_account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+twilio_phone_number = os.environ.get('TWILIO_PHONE_NUMBER')
 
 def send_sms(to, message):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    client = Client(twilio_account_sid, twilio_auth_token)
     message = client.messages.create(
         to=to,
-        from_=settings.TWILIO_PHONE_NUMBER,
+        from_=twilio_phone_number,
         body=message
     )
     return message.sid
